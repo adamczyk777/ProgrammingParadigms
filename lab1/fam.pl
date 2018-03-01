d3b5b34d3a48486bc401363549ed4619907fe346
Jakub Adamczyk <adamjaku@borg.kis.agh.edu.pl>
2018-03-01 12:21:58 +0100

    first lab

---
+++ lab1/fam.pl
@@ -0,0 +1,31 @@
+rodzic(kasia,robert).
+rodzic(tomek,robert).
+rodzic(tomek,eliza).
+rodzic(robert,anna).
+rodzic(robert,magda).
+rodzic(magda,jan).
+ 
+kobieta(kasia).
+kobieta(eliza).
+kobieta(magda).
+kobieta(anna).
+ 
+mezczyzna(tomek).
+mezczyzna(robert).
+mezczyzna(jan).
+
+matka(X,Y) :-
+	rodzic(X,Y),
+	kobieta(X).
+ 
+ojciec(X,Y) :-
+	rodzic(X,Y),
+	mezczyzna(X).
+
+przodek(X,Y) :-
+	rodzic(X,Y).
+ 
+przodek(X,Z) :-
+	rodzic(X,Y),
+	przodek(Y,Z).
+
