804f566f0a48a28a021fbe6a05d823b3d0172b9b
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-04-04 21:09:30 +0200

    Change folder structure

---
+++ prolog/lab3/listy.pl
@@ -0,0 +1,56 @@
+porownaj([_,_,D,D|_]).
+
+nalezy(X,[X|_]).
+nalezy(X,[_|Yogon]) :-
+	nalezy(X,Yogon).
+
+dlugosc([],0).
+dlugosc([_|Ogon],Dlug) :-
+	dlugosc(Ogon,X),
+	Dlug is X+1.
+
+a2b([],[]).
+a2b([a|Ta],[b|Tb]) :- 
+   a2b(Ta,Tb).
+
+sklej([],X,X).
+sklej([X|L1],L2,[X|L3]) :-
+	sklej(L1,L2,L3).
+
+nalezy1(X,L) :-
+	sklej(_,[X|_],L).
+
+ostatni(E,L) :-
+	sklej(_,[E],L).
+
+del3last(L1, L2) :-
+	sklej(L1, [_,_,_], L2).
+
+del3first(L1,L2) :-
+	sklej([_,_,_],L2,L1).
+
+parzysta([]).
+
+parzysta([_|L]) :- 
+	nieparzysta(L).
+
+nieparzysta([_|L]) :- 
+	parzysta(L).
+
+odwroc([],[]).
+odwroc([H|T],L) :-
+	odwroc(T,R),
+	sklej(R,[H],L).
+
+palindrom(L) :-
+	odwroc(L,L).
+
+przesun(L,L1) :-
+	sklej(L, [X], [X|L1]).
+
+przeloz([],[]).
+
+przeloz([A|L],[X|Y]) :-
+	znaczy(A,X).
+	przeloz(L,Y).
+	
