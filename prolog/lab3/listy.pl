766bdc6ac01f0c71a9c17f0471ced461b619f8aa
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-04-04 22:43:36 +0200

    learning

--- prolog/lab3/listy.pl
+++ prolog/lab3/listy.pl
@@ -1,46 +1,63 @@
 porownaj([_,_,D,D|_]).
 
+
+zamien([A,B,X,Y|C],[A,B,Y,X|C]).
+
+
 nalezy(X,[X|_]).
+
 nalezy(X,[_|Yogon]) :-
 	nalezy(X,Yogon).
+
 
 dlugosc([],0).
 dlugosc([_|Ogon],Dlug) :-
 	dlugosc(Ogon,X),
 	Dlug is X+1.
 
+
 a2b([],[]).
 a2b([a|Ta],[b|Tb]) :- 
    a2b(Ta,Tb).
+
 
 sklej([],X,X).
 sklej([X|L1],L2,[X|L3]) :-
 	sklej(L1,L2,L3).
 
+
 nalezy1(X,L) :-
 	sklej(_,[X|_],L).
+
 
 ostatni(E,L) :-
 	sklej(_,[E],L).
 
+
 del3last(L1, L2) :-
 	sklej(L1, [_,_,_], L2).
 
+
 del3first(L1,L2) :-
 	sklej([_,_,_],L2,L1).
+
 
 parzysta([]).
 
 parzysta([_|L]) :- 
 	nieparzysta(L).
 
+
 nieparzysta([_|L]) :- 
 	parzysta(L).
 
+
 odwroc([],[]).
+
 odwroc([H|T],L) :-
 	odwroc(T,R),
 	sklej(R,[H],L).
+
 
 palindrom(L) :-
 	odwroc(L,L).
@@ -48,9 +65,9 @@ palindrom(L) :-
 przesun(L,L1) :-
 	sklej(L, [X], [X|L1]).
 
+
 przeloz([],[]).
 
 przeloz([A|L],[X|Y]) :-
 	znaczy(A,X).
 	przeloz(L,Y).
-	
---
+++ prolog/usun.pl
@@ -0,0 +1,6 @@
+sklej([],X,X).
+sklej([X|L1],L2,[X|L3]) :-
+	sklej(L1,L2,L3).
+
+del3last(X) :-
+    sklej()
