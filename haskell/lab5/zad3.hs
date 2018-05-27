5edc852afc6f57cb6b9523dbe3a74ab049421b10
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-05-27 20:08:21 +0200

    zad 3

--- haskell/lab5/zad3.hs
+++ haskell/lab5/zad3.hs
@@ -2,4 +2,5 @@
 --funkcja zwróci dany element listy, lub Nothing jeśli predykat nie zakończy się True na żadnym z elementów listy.. 
 --Np możesz użyć predykatu porównującego osoby po peselu co umożliwi wyszukiwanie pierwszej osoby w liście o danym peselu.
 
-find :: ((a) -> Bool) -> [b] -> (b | Nothing)
+find :: (a -> Bool) -> [a] -> Maybe a
+find fun arr =
