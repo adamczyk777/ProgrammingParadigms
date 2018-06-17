b528c7516f0c4c88b24c909531bcd9229e0b858c
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-06-17 16:41:44 +0200

    add some excercises

--- haskell/kolokwium/zadania.hs
+++ haskell/kolokwium/zadania.hs
@@ -1,5 +1,5 @@
 import Data.List
-
+-------------------------------------------------------------------------------
 -- 1. Mamy dane typy danych:
 -- data Student = Student {id :: Int, name :: String, dateOfBirth :: String}
 -- data Score = Score {studentId :: Int, course1Score :: Int,
@@ -8,28 +8,121 @@ import Data.List
 -- i takie jak w typie danych Score (bez powtórzenia id). Uwaga na konflikt nazw w module! Jak można rozwiązać problem nazw?
 
 data Student = Student {
-  id :: Int, 
-  name :: String, 
-  dateOfBirth :: String
-}
+    id :: Int, 
+    name :: String, 
+    dateOfBirth :: String
+} deriving Show
 
 data Score = Score {
-  studentId :: Int,
-  course1Score :: Int,
-  course2Score :: Int,
-  course3Score :: Int
-}
+    studentId :: Int,
+    course1Score :: Int,
+    course2Score :: Int,
+    course3Score :: Int
+} deriving Show
 
-data StudentWithScores = Int String String Int Int Int deriving (Eq, Show)
+data StudentWithScores = StudentWithScores Int String String Int Int Int deriving (Eq, Show)
 
+-------------------------------------------------------------------------------
 -- 4. Dla przykładowych danych students i scores
 students :: [Student]
-students = [Student 26453 "Kristalee Copperwaite" "2000", Student 33596 "Roeberta Naden" "1997"]
+students = [Student 26453 "Kristalee Copperwaite" "2000", Student 33596 "Roeberta Naden" "1997", Student 35000 "Jakub Adamczyk" "1997"]
 scores :: [Score]
-scores = [Score 26453 93 97 80, Score 40241 86 85 87, Score 33596 82 60 80]
+scores = [Score 26453 93 97 80, Score 29970 93 96 80, Score 40241 86 85 87, Score 33596 82 60 80]
+
 -- zdefiniuj funkcję sortującą studentów po dacie urodzenia rosnąco.
 sortByDate :: [Student] -> [Student]
-sortByDate = 
+sortByDate = sortBy compareYears
+    where compareYears (Student _ _ date1) (Student _ _ date2) = compare (read date1 :: Int) (read date2 :: Int)
+
+
 -- zdefiniuj funkcję sortującą studentów po imieniu i nazwisku urodzenia malejąco.
+sortByName :: [Student] -> [Student]
+sortByName = sortBy compareNames
+    where compareNames (Student _ name1 _) (Student _ name2 _) = compare name1 name2
+
+
 -- zdefiniuj funkcję sortującą rekordy wyników tak, żeby rekordy były posortowane najpierw po wyniku z pierwszego kursu, a następnie (dla remisów) po id studneta
+sortByScores :: [Score] -> [Score]
+sortByScores = sortBy compare_
+    where compare_ (Score _ scoreA1 scoreB1 _) (Score _ scoreA2 scoreB2 _)
+            | scoreA1 == scoreA2 = compare scoreB1 scoreB2
+            | otherwise = compare scoreA1 scoreA2
+
+
 -- zdefiniuj funkcję sortującą rekordy wyników tak, żeby rekordy z większą sumaryczną ilością punktów były wcześniej od tych z mniejszą sumaryczną ilością punktów
+sortByScoresSum :: [Score] -> [Score]
+sortByScoresSum = sortBy compareSum
+    where compareSum (Score _ a1 b1 c1) (Score _ a2 b2 c2) = compare (a1 + b1 + c1) (a2 + b2 + c2)
+
+-------------------------------------------------------------------------------
+-- 5. Zdefiniuj fukcję o sygnaturze
+toStudentWithScores :: Student -> Score -> Maybe StudentWithScores
+-- która zwróci połączony rekord studenta z jego wynikami, 
+-- jeśli studentId jest równe id a Nothing w przeciwnym wypadku
+
+toStudentWithScores (Student id name date) (Score studentId sc1 sc2 sc3)
+    | studentId == id = Just $ StudentWithScores id name date sc1 sc2 sc3
+    | otherwise = Nothing
+
+-------------------------------------------------------------------------------
+--6. Zdefinuj funkcję o sygnatrzue 
+findById :: [Score] -> Int -> [Score] 
+-- która ma za zadnie zwrócić wyniki dla studenta o zadanym id (drugi parametr) 
+-- przy użyciu funkcji filter
+findById arr targetId = filter (\x -> studentId x == targetId) arr
+
+-------------------------------------------------------------------------------
+-- 7. Jeśli zdefinujemy funkcję 
+-- findBy f scrs = filter f scrs 
+-- to jaką sygnaturę będzie miała ta funkcja, jeśli scrs to [Score]?
+findBy :: (Score -> Bool) -> [Score] -> [Score]
+findBy f scrs = filter f scrs 
+
+-------------------------------------------------------------------------------
+-- 8. Jeśli zdefinujemy funkcję 
+-- mapBy f scrs = map f scrs 
+-- to jaką sygnaturę będzie miała ta funkcja, jeśli scrs to [Score]?
+mapBy :: (Score -> t) -> [Score] -> [t]
+mapBy f scrs = map f scrs
+
+-------------------------------------------------------------------------------
+-- 9. Jeśli zdefinujemy funkcję 
+-- reduceBy f scrs = foldl f [] scrs 
+-- to jaką sygnaturę będzie miała ta funkcja, jeśli scrs to [Score]?
+reduceBy :: ([t] -> Score -> [t]) -> [Score] -> [t]
+reduceBy f scrs = foldl f [] scrs
+
+-------------------------------------------------------------------------------
+-- 10. Przy użyciu funkcji map zdefinuj funkcję o sygnaturze
+mapToJoin :: Student -> [Score] -> [Maybe StudentWithScores]
+-- Wykorzystaj już zdefiniwane wcześniej funkcje
+
+
+-------------------------------------------------------------------------------
+-- 11. Zdefiniuj funkcję joinStep1 :: [Student] -> [Score] -> [(Student, [Score])]
+
+-------------------------------------------------------------------------------
+-- 12. Zdefiniuj funkcję joinStep2 :: [(Student, [Score])] -> [[Maybe StudentWithScores]]
+
+-------------------------------------------------------------------------------
+-- 13. Zdefiniuj funkcję joinStep3 :: [[Maybe StudentWithScores]] -> [StudentWithScores]
+
+-------------------------------------------------------------------------------
+-- 14. Zdefiniuj funkcję join :: [Student] -> [Score] -> [StudentWithScores]
+
+-------------------------------------------------------------------------------
+-- 15. Zdefiniuj klasę Id która rozszerza klasę Ord i zawiera pojedyncza metodę toInt konwertującą daną implementację na typ Int
+
+-------------------------------------------------------------------------------
+-- 16. Zdefiniuj klasę HasId zawiera pojedyncza metodę getId konwertującą daną implementację na dane typu Id
+
+-------------------------------------------------------------------------------
+-- 17. Zdefiniuj klasę Repository zawierające operacje insert, delete, get, update i count. insert pobiera element implementujący HasId i implementacje repozytorium i zwraca nowe repozytorium. delete pobiera id należące do klasy Id i implementacje repozytorium i zwraca nowe repozytorium get pobiera id należące do klasy Id i implementacje repozytorium i zwraca Maybe element należący do klasy HasId. update pobiera id należące do klasy Id, element implementujący HasId i implementacje repozytorium i zwraca nowe repozytorium. count pobierające implementacje repozytorium i zwraca Int
+
+-------------------------------------------------------------------------------
+-- 18. Zdefiniuj typ InMemoryRepository który posiada jedno pole typu Map key value i należy do klasy Repository
+-- insert :: Ord k => k -> a -> Map k a -> Map k a
+-- Data.Map.delete :: Ord k => k -> Map k a -> Map k a
+-- Data.Map.alter :: Ord k => (Maybe a -> Maybe a) -> k -> Map k a -> Map k a
+-- Data.Map.lookup :: Ord k => k -> Map k a -> Maybe a
+-- Data.Map.size :: Map k a -> Int
